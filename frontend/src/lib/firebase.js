c99fec973a59ea7378345abd984b1ed41ec9c1f6
unknown <smitpotkar20@gmail.com>
2025-05-06 18:50:16 +0530

    ENV variables for Firebase Config

--- frontend/src/lib/firebase.js
+++ frontend/src/lib/firebase.js
@@ -1,19 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAYKTMxQ9AO83FU_IgsunZRAimA-MAoTCs",
-  authDomain: "taskcircle-63837.firebaseapp.com",
-  projectId: "taskcircle-63837",
-  storageBucket: "taskcircle-63837.firebasestorage.app",
-  messagingSenderId: "39129350188",
-  appId: "1:39129350188:web:d5b996e604973fd4eb9189",
-  measurementId: "G-J9S1JNFDPX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
